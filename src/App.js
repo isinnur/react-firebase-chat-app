67ff47f07a03a9775c5139d8d9e9be3bbc40ce8e
isinnur <isinngnyy@gmail.com>
2023-11-03 12:19:46 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./style.scss";
+import Register from "./pages/Register.jsx";
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">CHAT CHAT CHAT</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">sasasa</label>
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+}
