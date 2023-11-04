8a924f951560236d19fc00b8291f785197e470fa
isinnur <isinngnyy@gmail.com>
2023-11-04 10:04:06 +0300

    feat: add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="container">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
