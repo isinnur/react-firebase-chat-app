937eca6466ae0e47095e8730e5d555f53cb72769
isinnur <isinngnyy@gmail.com>
2023-11-28 12:02:32 +0300

    feat: fetch private messages

---
+++ src/context/ChatContext.js
@@ -0,0 +1,44 @@
+import { onAuthStateChanged } from "firebase/auth";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { auth } from "../firebase";
+import { AuthContext } from "./AuthContext";
+
+export const ChatContext = createContext(); //kullanıcı oturum açma durumunu izler
+
+const { currentUser } = useContext(AuthContext);
+
+export const ChatContextProvider = ({ children }) => {
+  const INITIAL_STATE = {
+    chatId: "null",
+    user: {},
+  };
+
+  const chatReducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE_USER":
+        return {
+          user: action.payload,
+          chatId:
+            currentUser.uid > action.payload.uid
+              ? currentUser.uid + action.payload.uid
+              : action.payload.uid + currentUser.uid,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+
+  return (
+    <ChatContext.Provider value={{ data: state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContext, ChatContextProvider } from "./context/ChatContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
